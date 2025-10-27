6d977d7dc8ac21d9a541878c14e68837d09a5c53
d4yuu <willycornelius555@gmail.com>
2025-10-27 14:22:42 +0700

    Implement CRUD operations for expenses
    Added methods to add, retrieve, delete, and edit expenses in the ExpenseData class.

--- lib/expense_data.dart
+++ lib/expense_data.dart
@@ -4,21 +4,25 @@ import 'expense_model.dart';
 class ExpenseData {
   static const String boxName = "expensesBox";
 
+  //nambahkan expense dengan id
   static Future<void> addExpense(ExpenseModel expense) async {
     final box = Hive.box<ExpenseModel>(boxName);
     await box.put(expense.id, expense);
   }
 
+  //mengambil data expense
   static List<ExpenseModel> getExpenses() {
     final box = Hive.box<ExpenseModel>(boxName);
     return box.values.toList();
   }
 
+  //menghapus expense
   static Future<void> deleteExpense(String id) async {
     final box = Hive.box<ExpenseModel>(boxName);
     await box.delete(id);
   }
 
+  //edit expense
   static Future<void> editExpense(ExpenseModel updated) async {
     final box = Hive.box<ExpenseModel>(boxName);
     await box.put(updated.id, updated);
